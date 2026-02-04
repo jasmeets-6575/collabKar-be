7415d453aba7f905be662dc3516367bb00b0fb2a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2026-02-04 22:54:54 +0530

    fix apierror

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,10 +1,5 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = "Something went wrong",
-        errors = [],
-        stack = ""
-    ) {
+    constructor(statusCode, message = "Something went wrong", errors = [], stack = "") {
         super(message);
         this.statusCode = statusCode;
         this.data = null;
@@ -17,6 +12,18 @@ class ApiError extends Error {
         } else {
             Error.captureStackTrace(this, this.constructor);
         }
+    }
+
+    static handleError(res) {
+        return (err) => {
+            const statusCode = err.statusCode || 500;
+            const message = err.message || 'Internal Server Error';
+            return res.status(statusCode).json({
+                data: null,
+                error: message,
+                success: false
+            });
+        };
     }
 }
 
