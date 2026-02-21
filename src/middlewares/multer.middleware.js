968adfeadae8e5396a0ff8d861de33dc8b4bf1ff
Jasmeet Singh <jasmeetsonu990@gmail.com>
2026-02-21 05:30:30 +0530

    Image Issue Fix

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,9 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const tempDir = path.resolve(process.cwd(), "public/temp");
+fs.mkdirSync(tempDir, { recursive: true });
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
